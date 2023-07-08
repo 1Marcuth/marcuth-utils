8da44662aece3af75d4b8f773beb7c5a148546fb
TantoFaz <thiago.playgames15@gmail.com>
2023-07-08 08:17:09 -0300

    -

--- src/components/safepro-ad.jsx
+++ src/components/safepro-ad.jsx
@@ -5,7 +5,7 @@ const SurfeProAd = () => {
         <Fragment>
             <iframe style={{
                 width: "100%",
-                height: "100%",
+                height: "250",
                 display: "flex",
                 justifyContent: "center",
                 alignItems: "center"
